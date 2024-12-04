e1c5cb80743ffc2cf945b2706da54f809e17d092
SzymonPniaczek <spniaczek@proton.me>
2024-12-04 13:57:03 +0100

    test

--- src/requests/updateToDo.js
+++ src/requests/updateToDo.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const updateTodo = async (id, todo) => {
   const response = await axios.put(
-    `https://todo-backend-one-ebon.vercel.app/${id}`,
+    `https://todo-backend-one-ebon.vercel.app/todo/${id}`,
     todo
   );
   return await response.data;
